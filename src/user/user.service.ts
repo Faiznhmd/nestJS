50564bd0039721aabec327003ffc4c028bd6420b
Faizan Ahmad <fiaznhmd.0624@gmail.com>
2026-02-06 19:50:21 +0530

    error handling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { RegisterDto } from 'src/auth/dto/register.user.dto';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
@@ -8,11 +8,21 @@ import { InjectModel } from '@nestjs/mongoose';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   async user(registerDto: RegisterDto) {
-    return await this.userModel.create({
-      fname: registerDto.fname,
-      lname: registerDto.lname,
-      email: registerDto.email,
-      password: registerDto.password,
-    });
+    try {
+      return await this.userModel.create({
+        fname: registerDto.fname,
+        lname: registerDto.lname,
+        email: registerDto.email,
+        password: registerDto.password,
+      });
+    } catch (err: unknown) {
+      console.log(err);
+      const e = err as { code?: number };
+      const DUPLICATE_KEY_CODE = 11000;
+      if (e.code === DUPLICATE_KEY_CODE) {
+        throw new ConflictException('Email is already Taken');
+      }
+      throw err;
+    }
   }
 }
